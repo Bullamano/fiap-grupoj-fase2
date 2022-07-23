886ac8ea90eec2689e51bffb149869b9c1faa943
Allan <allanblamano@gmail.com>
2022-07-23 17:38:48 -0300

    Criação de projeto Flutter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fiap-grupoj-fase2
+
 Repositório para entrega da atividade da fase 2 do MBA de Full Stack Development da FIAP, grupo J da turma MBA5SCJO.
---
+++ android/app/src/main/kotlin/br/com/fiap/mba/grupoj/need_help/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.fiap.mba.grupoj.need_help
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
